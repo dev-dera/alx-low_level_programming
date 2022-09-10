2db98c23d82dedbcdeacc58f1591e55d0c3a167e
dev-dera <ihemefc01@gmail.com>
2022-09-09 19:44:15 -0700

    c_language variables task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,22 @@
-Executable File  0 lines (0 sloc)  0 Bytes
+#include <stdio.h>
+/**
+ *main - main block
+ *Description:  prints the alphabet in lowercase,
+ *and then in uppercase, followed by a new line.
+ *Return: 0
+ */
+int main(void)
+{
+		char ch;
+			for (ch = 'a' ; ch <= 'z' ; ch++)
+		{
+				putchar(ch);
+		}
+			for (ch = 'A' ; ch <= 'Z' ; ch++)
+		{
+				putchar(ch);
+													}
+		putchar('\n');
+		return (0);
+}
+
