a051eea6357aa69f1cf79c66db86f11bba33fca2
dev-dera <ihemefc01@gmail.com>
2022-09-13 18:15:59 -0700

    debugging task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,21 @@
-Executable File  0 lines (0 sloc)  0 Bytes
+#include <stdio.h>
+
+/**
+ *  main - Entry Point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+		int digit;
+		char alpha;
+
+	for (digit = 0; digit <= 9; digit++)
+							putchar(digit + '0');
+		for (alpha = 'a'; alpha <= 'f'; alpha++)
+			putchar(alpha);
+
+		putchar('\n');
+
+		return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,25 @@
-Executable File  0 lines (0 sloc)  0 Bytes
+#include <stdio.h>
+
+/**
+ *  * main - Entry Point
+ *   *
+ *    * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+		int digit;
+
+			for (digit = 0; digit <= 9; digit++)
+					{
+								putchar(digit + '0');
+										if (digit == 9)
+														break;
+												putchar(',');
+														putchar(' ');
+															}
+
+				putchar('\n');
+
+					return (0);
+}
