36559ef81efa3c6dfdbda8a18fc1dc7ce75bd4f1
dev-dera <ihemefc01@gmail.com>
2022-09-16 16:10:17 -0700

    more C-functions and nested loops projects

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-  *more_numbers - prints numbers to 14
-  *
-  *Return: returns nothing
-  */
+*more_numbers - prints numbers to 14
+*
+*Return: returns nothing
+*/
 void more_numbers(void)
 {
 	int number1, number2;
@@ -12,16 +12,12 @@ void more_numbers(void)
 	{
 		for (number2 = 0; number2 <= 14; number2++)
 		{
-
 			if (number2 > 9)
 			{
-
 				_putchar((number2 / 10) + '0');
-
 			}
 			_putchar((number2 % 10) + '0');
 		}
 		_putchar(10);
 	}
-
 }
